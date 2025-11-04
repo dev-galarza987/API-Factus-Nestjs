5c2088dc64231504cea2a91397de7075090ebdeb
Victor Hugo Galarza Jimenez <dev.galarza@gmail.com>
2025-11-03 21:14:16 -0400

    feat(invoice): integrate TypeOrmModule for Invoice, InvoiceDetail, Company, and Customer entities.

--- src/invoice/invoice.module.ts
+++ src/invoice/invoice.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { InvoiceService } from './invoice.service';
 import { InvoiceController } from './invoice.controller';
+import { Invoice } from './entities/invoice.entity';
+import { InvoiceDetail } from 'src/invoice-detail/entities/invoice-detail.entity';
+import { Company } from 'src/company/entities/company.entity';
+import { Customer } from 'src/customer/entities/customer.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Invoice, InvoiceDetail, Company, Customer]),
+  ],
   controllers: [InvoiceController],
   providers: [InvoiceService],
+  exports: [InvoiceService],
 })
 export class InvoiceModule {}
