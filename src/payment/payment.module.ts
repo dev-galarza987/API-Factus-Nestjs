6cae93fbb6fc438d80920d480d1112e3ab26687d
Victor Hugo Galarza Jimenez <dev.galarza@gmail.com>
2025-11-03 21:29:06 -0400

    feat(payment): integrate TypeOrmModule for Payment and Invoice entities

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
+import { Payment } from './entities/payment.entity';
+import { Invoice } from 'src/invoice/entities/invoice.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Payment, Invoice])],
   controllers: [PaymentController],
   providers: [PaymentService],
+  exports: [PaymentService],
 })
 export class PaymentModule {}
