771ce14f0783ace79fa57e55b50f17694e637783
Victor Hugo Galarza Jimenez <dev.galarza@gmail.com>
2025-11-03 21:20:39 -0400

    feat(invoice-detail): integrate TypeOrmModule for InvoiceDetail and Invoice entities.

--- src/invoice-detail/invoice-detail.module.ts
+++ src/invoice-detail/invoice-detail.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { InvoiceDetailService } from './invoice-detail.service';
 import { InvoiceDetailController } from './invoice-detail.controller';
+import { InvoiceDetail } from './entities/invoice-detail.entity';
+import { Invoice } from 'src/invoice/entities/invoice.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([InvoiceDetail, Invoice])],
   controllers: [InvoiceDetailController],
   providers: [InvoiceDetailService],
+  exports: [InvoiceDetailService],
 })
 export class InvoiceDetailModule {}
