8155ec6eb3e3fbb14968fba4541ab1b6e840ee24
Victor Hugo Galarza Jimenez <dev.galarza@gmail.com>
2025-11-03 23:41:07 -0400

    feat(app): add HomeModule and ApiModule to the application imports.

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,8 @@ import { CustomerModule } from './customer/customer.module';
 import { InvoiceModule } from './invoice/invoice.module';
 import { InvoiceDetailModule } from './invoice-detail/invoice-detail.module';
 import { PaymentModule } from './payment/payment.module';
+import { HomeModule } from './home/home.module';
+import { ApiModule } from './api/api.module';
 
 @Module({
   imports: [
@@ -23,6 +25,8 @@ import { PaymentModule } from './payment/payment.module';
       entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
+    HomeModule,
+    ApiModule,
     CompanyModule,
     CustomerModule,
     InvoiceModule,
